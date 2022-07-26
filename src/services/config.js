856e2252186177f414cfbfe49463312a2df9b39c
shaoxueping <shaoxueping@bytedance.com>
2022-07-26 14:59:36 +0800

    feat: 修改config

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-const devBaseURL = "http://81.69.228.97:3000/";
-const proBaseURL = "http://123.207.32.32:9001/";
+const devBaseURL = 'http://81.69.228.97:3000/'
+// const proBaseURL = "http://123.207.32.32:9001/";
 export const BASE_URL = devBaseURL
 
-export const TIMEOUT = 5000;
+export const TIMEOUT = 5000
